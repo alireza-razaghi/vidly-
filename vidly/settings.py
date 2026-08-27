7ab39c34efed78a5488a2625504aee1fc2ca252a
alireza-razaghi <rrzalireza@gmail.com>
2026-08-27 14:43:42 +0330

    Fix DATABASE_URL env variable reference

--- vidly/settings.py
+++ vidly/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +78,10 @@ WSGI_APPLICATION = 'vidly.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=200,
+    )
 }
 
 
@@ -128,4 +130,3 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
-ALLOWED_HOSTS = ['web-production-1bf1f.up.railway.app', 'localhost', '127.0.0.1']
